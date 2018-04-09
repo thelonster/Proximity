505d9718a598c27839d81529f8f5c645a97f4026
Lonnie Hansen <lg3hansen@gmail.com>
2018-04-09 00:28:08 -0700

    Updating the geolocation

--- angular-src/src/app/components/chat-room/geolocation.js
+++ angular-src/src/app/components/chat-room/geolocation.js
@@ -3,36 +3,59 @@ Our Google API key: AIzaSyCCMdJYDCf_gZ5O9AODdeEe1NMBXx9jj8w
 */
 //Gets the user's location
 var userCounty;
-const var countyList - = ["Los Angeles County","Orange County","Riverside County","San Diego County","San Bernardino County",
+var countyList = ["Los Angeles County","Orange County","Riverside County","San Diego County","San Bernardino County",
                          "Kern County","Ventura County","Santa Barbara County","San Luis Obispo County","Imperial County"];
+var pos;
+var crd;
+var longi = 33.8728111;
+var lati = -117.84871449999999;
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
 
 function getLocation() {
     if (navigator.geolocation) {
-        return navigator.geolocation.getCurrentPosition();
+        navigator.geolocation.getCurrentPosition(savePosition,error,options);
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
 
-function decodeGeoLocation(geocoder, position) {
+function savePosition(position) {
+pos = position;
+crd = pos.coords;
+longi = crd.longitude;
+lati = crd.latitude;
+}
+
+function error() {
+    alert("Unable to get your location");
+}
+
+
+function decodeGeoLocation(geocoder) {
     //position from getLocation used to get lat and long
-    var latlng = {lat: position.coords.latitude, lng: position.coords.longitude}
+    var latlng = {lat: lati, lng: longi}
     //uses the lat and long to decode the location and places a list of addresses into results
     geocoder.geocode({'location': latlng}), function(results, status) {
         if (status == 'OK') {
             //finds the address that includes the County and State
             var countyaddress = 'NONE';
             for (var i = 0; i < results.length; i++) {
-                if (results[i].formatted_address.indexOf('County') != -1 )
+                if (results[i].formatted_address.indexOf('County') != -1 ) {
                     countyaddress = results[i].formatted_address;
+				}
             }
             //finds the county
             var county = 'NONE';
             var add_comps = results.address_components;
             for (var i = 0; i < add_comps.length; i++) {
                 for (var j = 0; j > add_comps.types.length; j++) {
-                   if (add_comps.types[j] = "administrative_area_level_2")
+                   if (add_comps.types[j] = "administrative_area_level_2") {
                       county = add_comps[i].long_name;
+				  }
                 }
             }
             if (county != 'NONE')
@@ -60,7 +83,7 @@ function manualUserPrompt() {
 function checkCounty(var tCounty) {
     var found = false;
     for (var c = 0; c < countyList.length; c++) {
-        if (tCounty == manCounty[c])
+        if (tCounty == countyList[c])
             found = true;
     }
     return found;
