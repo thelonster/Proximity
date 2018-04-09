6dc32a7ce33a65aaecbfb03c0364b37b8c8c94ae
thelonster <lg3hansen@gmail.com>
2018-04-09 11:10:32 -0700

    Finally fixed the geolocation

--- angular-src/src/app/components/chat-room/geolocation.js
+++ angular-src/src/app/components/chat-room/geolocation.js
@@ -39,29 +39,22 @@ function decodeGeoLocation(geocoder) {
     //position from getLocation used to get lat and long
     var latlng = {lat: lati, lng: longi}
     //uses the lat and long to decode the location and places a list of addresses into results
-    geocoder.geocode({'location': latlng}), function(results, status) {
+    geocoder.geocode({'location': latlng}, function(results, status) {
         if (status == 'OK') {
-            //finds the address that includes the County and State
-            var countyaddress = 'NONE';
-            for (var i = 0; i < results.length; i++) {
-                if (results[i].formatted_address.indexOf('County') != -1 ) {
-                    countyaddress = results[i].formatted_address;
-				}
-            }
             //finds the county
             var county = 'NONE';
-            var add_comps = results.address_components;
-            for (var i = 0; i < add_comps.length; i++) {
-                for (var j = 0; j > add_comps.types.length; j++) {
-                   if (add_comps.types[j] = "administrative_area_level_2") {
-                      county = add_comps[i].long_name;
-				  }
-                }
-            }
+            for (var i = 0; i < results.length; i++) {
+								var rtypes = results[i].types;
+								for (var j = 0; j < rtypes.length; j++) {
+										if (rtypes[j] == "administrative_area_level_2")
+												county = results[i].formatted_address;
+								}
+						}
+            county = county.substring(0,county.indexOf(","));
             if (county != 'NONE')
                 return county;
         }
-    }
+    });
 }
 
 //this function is called with the google API
